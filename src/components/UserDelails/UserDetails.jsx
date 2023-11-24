1b385df37347b4813171622ea1be64320a5dc74f
Alaul1988 Sunan <alaul1988sunan@gmail.com>
2023-11-24 14:22:07 -0500

    dynamic-user-added

---
+++ src/components/UserDelails/UserDetails.jsx
@@ -0,0 +1,19 @@
+import { useLoaderData } from "react-router-dom";
+
+
+const UserDetails = () => {
+    const user = useLoaderData();
+    const {name, phone, website, company} = user;
+    return (
+
+        <div>
+            <h2>Details about users: {name}</h2>
+            <p>Phone: {phone}</p>
+            <p>Website: {website}</p>
+           
+            
+        </div>
+    );
+};
+
+export default UserDetails;
